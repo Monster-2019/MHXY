bf1aaa3df890d97b4ae7bacb88ff19e5ca78f16d
dongxin <dongxin2019@gmail.com>
2022-08-29 09:27:10 +0800

    1

--- config/user.py
+++ config/user.py
@@ -1,34 +1,42 @@
 DEFAULT_CONFIG = {
-    'TeamStatus': False,
-    'ZG': False,
-    'ZG_COUNT': 2,
-    'ZG_WC': None,
-    'FB': False,
-    'FB_WC': None,
-    'NEXT': False
+    "TeamStatus": False,
+    "ZG": False,
+    "ZG_COUNT": 2,
+    "ZG_WC": None,
+    "FB": False,
+    "FB_WC": None,
+    "NEXT": True,
 }
 
 ACCTZU = [
     {
-        "status": False,
-        "config": DEFAULT_CONFIG,
-        "acctList": [
-            { 'account': "h1", 'server': "h1-1" },
-            { 'account': "h2", 'server': "h2-1" },
-            { 'account': "h3", 'server': "h3-1" },
-            { 'account': "h4", 'server': "h4-1" },
-            { 'account': "h5", 'server': "h5-1" },
-        ]
-    },
-    {
         "status": True,
         "config": DEFAULT_CONFIG,
         "acctList": [
-            { 'account': "h1", 'server': "h1-2" },
-            { 'account': "h2", 'server': "h2-2" },
-            { 'account': "h3", 'server': "h3-2" },
-            { 'account': "h4", 'server': "h4-2" },
-            { 'account': "h5", 'server': "h5-2" },
-        ]
-    }
+            {"account": "h1", "server": "h1-1"},
+            {"account": "h2", "server": "h2-1"},
+            {"account": "h3", "server": "h3-1"},
+            {"account": "h4", "server": "h4-1"},
+            {"account": "h5", "server": "h5-1"},
+        ],
+    },
+    {
+        "status": True,
+        "config": {
+            "TeamStatus": False,
+            "ZG": True,
+            "ZG_COUNT": 2,
+            "ZG_WC": None,
+            "FB": False,
+            "FB_WC": None,
+            "NEXT": True,
+        },
+        "acctList": [
+            {"account": "h1", "server": "h1-2"},
+            {"account": "h2", "server": "h2-2"},
+            {"account": "h3", "server": "h3-2"},
+            {"account": "h4", "server": "h4-2"},
+            {"account": "h5", "server": "h5-2"},
+        ],
+    },
 ]
