cd6b5e3f38d912e90d8fb8e51e3531f13e8d4908
Monster <dongxin2019@gmail.com>
2023-04-03 14:45:13 +0800

    debug

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+ls
+
 pip install pyinstaller
 
 python -m eel main.py web/dist --add-data "images;images" --add-data "config.ini;config.ini" --noconfirm --noconsole --name mhxy_script
 
 if [[ ! $(uname -sr) == MINGW* ]]; then
     echo not windows
-    cd dist/
+    cd dist
     zip -r mhxy_script.zip mhxy_script
     cd ..
 else 
