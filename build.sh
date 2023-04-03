83dc44b2b4c4d8d1985b253f89089c9c21ee0c89
Monster <dongxin2019@gmail.com>
2023-04-03 16:12:20 +0800

    3

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ if [[ ! $(uname -sr) == MINGW* ]]; then
     echo not windows
     pwd
     python -m eel main.py web/dist --add-data "images:images" --add-data "config.ini:." --noconfirm --noconsole --name mhxy_script
-    ls
-    cd dist
-    ls
-    zip mhxy_script.zip mhxy_script
+    cat dist
+    # cd dist
+    # ls
+    # zip mhxy_script.zip mhxy_script
 else 
     python -m eel main.py web/dist --add-data "images;images" --add-data "config.ini;." --noconfirm --noconsole --name mhxy_script
     echo is windows
