80892ae842952447ad00bc6b43aa98d1044a1619
Monster <dongxin2019@gmail.com>
2023-04-03 15:56:26 +0800

    4

--- build.sh
+++ build.sh
@@ -4,10 +4,8 @@ python -m eel main.py web/dist --add-data "images:images" --add-data "config.ini
 
 if [[ ! $(uname -sr) == MINGW* ]]; then
     echo not windows
-    folder="dist"
-    if [ ! -d "$folder"]; then
-        echo not dist
-    fi
+    ls
+    cd dist
     ls
     zip -r mhxy_script.zip . -i dist/mhxy_script
 else 
