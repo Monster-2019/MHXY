90f754bbd18507d87e0c0aa3a2c222a8b55eb1c5
Monster <dongxin2019@gmail.com>
2023-04-03 13:37:36 +0800

    不更新pip，重试

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+pip install pyinstaller
+
 python -m eel main.py web/dist --add-data "images;images" --add-data "config.ini;config.ini" --noconfirm --noconsole --name mhxy_script
 
 if [[ ! $(uname -sr) == MINGW* ]]; then
