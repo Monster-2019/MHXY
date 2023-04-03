a4dec0e2ee11b3a3218d812dd5974d8b777646eb
Monster <dongxin2019@gmail.com>
2023-04-03 15:13:21 +0800

    debug

--- build.sh
+++ build.sh
@@ -4,7 +4,9 @@ python -m eel main.py web/dist --add-data "images:images" --add-data "config.ini
 
 if [[ ! $(uname -sr) == MINGW* ]]; then
     echo not windows
+    ls
     cd dist
+    ls
     zip -r mhxy_script.zip mhxy_script
     cd ..
 else 
